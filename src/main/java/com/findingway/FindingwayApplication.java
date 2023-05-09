72423e1761f7ac41340c7eb5e8624145b8b8b2d9
Chaseoungjun <chas369@naver.com>
2023-05-09 19:35:11 +0900

    basic setting

---
+++ src/main/java/com/findingway/FindingwayApplication.java
@@ -0,0 +1,13 @@
+package com.findingway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindingwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindingwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/findingway/FindingwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findingway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindingwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
