eddc42aeb70c8028165511e063337e96150834bc
Chaseoungjun <chas369@naver.com>
2023-05-20 21:04:39 +0900

    PharmacyRepository 생성

---
+++ src/main/java/com/findingway/pharmacy/repository/PharmacyRepository.java
@@ -0,0 +1,9 @@
+package com.findingway.pharmacy.repository;
+
+import com.findingway.pharmacy.entity.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+
+}
