da0d757bc0b6634a4e903a4aa5249bf4be0b11ce
Chaseoungjun <chas369@naver.com>
2023-05-21 22:02:02 +0900

    사용자로부터 입력받은 주소를 바탕으로 카카오 API를 사용하여 약국을 찾는 서비스를 생성 지도 url과 로드뷰 url을 생성하여 반환한다.

---
+++ src/main/java/com/findingway/pharmacy/service/PharmacyRecommendationService.java
@@ -0,0 +1,67 @@
+package com.findingway.pharmacy.service;
+
+import com.findingway.api.dto.KakaoDocumentDto;
+import com.findingway.api.dto.KakaoResponseDto;
+import com.findingway.api.service.KakaoAddressSearchService;
+import com.findingway.direction.dto.PharmacyInfoDto;
+import com.findingway.direction.entity.Direction;
+import com.findingway.direction.service.Base62Service;
+import com.findingway.direction.service.DirectionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PharmacyRecommendationService {
+
+    private final KakaoAddressSearchService addressSearchService;
+    private final DirectionService directionService;
+
+    private final Base62Service base62Service;
+
+    private static final String ROAD_VIEW_BASE_URL = "https://map.kakao.com/link/roadview/";
+    @Value("{pharmacy.recommendation.base.url}")
+    private String baseUrl;
+
+
+    public List<PharmacyInfoDto> recommendPharmacy(String address) {
+        KakaoResponseDto kakaoResponseDto = addressSearchService.requestAddressSearch(address);
+         // 카카오 API를 사용해서 입력받은 주소를 위치기반 데이터로 변경
+
+        // retry의 결과로 null이 반환된 경우와 검색 결과로 아무것도 나오지 않은 경우
+        if (Objects.isNull(kakaoResponseDto) || CollectionUtils.isEmpty(kakaoResponseDto.getDocumentDtoList())) {
+            log.info("PharmacyRecommendationService recommendPharmacy fail input address: {}", address);
+            return Collections.emptyList();
+        }
+
+        KakaoDocumentDto documentDto = kakaoResponseDto.getDocumentDtoList().get(0);
+//        List<Direction> pharmacyList = directionService.searchPharmacyWithAddress(documentDto);  // 공공데이터 사용
+        List<Direction> pharmacyList = directionService.searchPharmacyByCategoryApi(documentDto);
+
+        List<Direction> directionList = directionService.saveAll(pharmacyList);
+        return directionList.stream()
+                .map(direction -> convertToPharmacyInfo(direction))
+                .collect(Collectors.toList());
+    }
+
+    private PharmacyInfoDto convertToPharmacyInfo(Direction direction) {
+
+        return PharmacyInfoDto.builder()
+                .pharmacyAddress(direction.getTargetAddress())
+                .pharmacyName(direction.getTargetPharmacyName())
+                .directionUrl(baseUrl + base62Service.encodeDirectionId(direction.getId())) // shorten url
+                .roadViewUrl(ROAD_VIEW_BASE_URL + direction.getTargetLatitude() + "," + direction.getTargetLongitude())
+                .distance(String.format("%.2f km", direction.getDistance()))  // 소수점 2번째 자리까지
+                .build();
+    }
+
+}
