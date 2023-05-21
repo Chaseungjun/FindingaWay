4bae337fc9495f1d08980cad950e79f6340aece2
Chaseoungjun <chas369@naver.com>
2023-05-21 15:27:05 +0900

    카카오 지도 API에 맞는 url를 생성하는 서비스

--- src/main/java/com/findingway/api/service/KakaoUriBuilderService.java
+++ src/main/java/com/findingway/api/service/KakaoUriBuilderService.java
@@ -11,17 +11,37 @@ import java.net.URI;
 public class KakaoUriBuilderService {
 
     private static final String KAKAO_LOCAL_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
+    private static final String KAKAO_LOCAL_CATEGORY_SEARCH_URL = "https://dapi.kakao.com/v2/local/search/category.json";
 
     public URI buildUriByAddressSearch(String address){
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(KAKAO_LOCAL_SEARCH_ADDRESS_URL);
         uriBuilder.queryParam("query", address);
 
-        URI uri = uriBuilder.build().encode().toUri(); //  한글 주소를 입력받고 UTF-8로 인코딍
+        URI uri = uriBuilder.build().encode().toUri(); //  한글 주소를 입력받고 UTF-8로 인코딩
         log.info("[KakaoUriBuilderService buildUriByAddressSearch] address: {}, uri: {}", address, uri);
 
         return uri;
     }
 
 
+    /**
+     * 사용자가 입력한 주소를 위도, 경도로 데이터로 변경하고 반경 내의 요구 사항을 찾는다
+     */
+    public URI buildUriByCategorySearch(double latitude, double longitude, double radius, String category) {
 
+        double meterRadius = radius * 1000;  // api마다 단위가 다르므로 주의해야한다 카테고리로 검색하는 api는 단위가 미터
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(KAKAO_LOCAL_CATEGORY_SEARCH_URL);
+        uriBuilder.queryParam("category_group_code", category);
+        uriBuilder.queryParam("x", longitude);
+        uriBuilder.queryParam("y", latitude);
+        uriBuilder.queryParam("radius", meterRadius);
+        uriBuilder.queryParam("sort","distance");
+
+        URI uri = uriBuilder.build().encode().toUri();
+
+        log.info("[KakaoAddressSearchService buildUriByCategorySearch] uri: {} ", uri);
+
+        return uri;
+    }
 }
